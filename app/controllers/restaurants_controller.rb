97ccca06d2b16dd4bf503dbef4d74493d8d0b6b3
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-18 14:38:34 +0200

    add simple form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,42 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :edit, :update, :destroy, :show ]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+  end
+
+  def destroy
+    @restaurant.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to restaurant_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permits(:name, :address, :phone_number, :category)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.s.draw do
+Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :new, :create, :edit, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
